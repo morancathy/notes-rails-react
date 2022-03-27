de6bc5fac330832e93ae06a190813556362568ef
Cat <cathy.moran@teladochealth.com>
2022-03-26 23:55:12 -0500

    update user controller to include card_details tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
+    has_many :card_details
     has_secure_password   #this is a functin, dont need (). This comes from bcrypt library
 end
